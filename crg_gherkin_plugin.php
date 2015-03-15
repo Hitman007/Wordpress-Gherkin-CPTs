77ca89d7d7b832d5b6827e59c939a6f10a184e58
James Karlinski <support@customrayguns.com>
2015-03-15 12:44:19 +0000

    crg_gherkin_plugin.php edited online with Bitbucket

--- crg_gherkin_plugin.php
+++ crg_gherkin_plugin.php
@@ -9,33 +9,33 @@ Author URI: http://customrayguns.com
 Text Domain: CRG_Gherkin
 */
 
-//Load Custom Ray Gun's Gherkin CPTs:
-//include_once(plugin_dir_path( __FILE__ ) . "methods/CRGcreateGherkCPTs.php" );
-
 //This loads the classes nessicary for this plugin:
 function CRGautoloader($class) {
     include 'classes/' . $class . '.class.php';
 }
 // <^-- END //This loads the classes nessicary for this plugin:
 
+//Loads the CPT and the activation hook:
 CRGautoloader("CRG_GherkinCustomPostTypes");
 $CRG_GherkinCustomPostTypes = new CRG_GherkinCustomPostTypes;
 register_activation_hook(__FILE__,array($CRG_GherkinCustomPostTypes,'activate'));
 
-//If a pickle is being submitted, then launch the Pickle Receiver
+//If a pickle is being submitted, then launch the Pickle Receiver:
 if (isset($_POST['CRG_Gherkin_form_submit_button'])){
 	CRGautoloader("CRG_GherkinReceiver");
 	$CRG_GherkinReceiver = new CRG_GherkinReceiver;
 }
 
+//Loads the CSS:
 wp_register_style( 'CRG Gherkin Plugin CSS', plugins_url( '/crg_gherkin/css/crg_gherkin_plugin.css' ) );
 wp_enqueue_style( 'CRG Gherkin Plugin CSS' );
-	wp_register_script('CRG Gherkin Plugin JS', plugins_url( '/crg_gherkin/js/crg_gherkin.js' ), "jquery-core" );
-	wp_enqueue_script( 'CRG Gherkin Plugin JS' );
+
+//Loads jQuery:
+wp_register_script('CRG Gherkin Plugin JS', plugins_url( '/crg_gherkin/js/crg_gherkin.js' ), "jquery-core" );
+wp_enqueue_script( 'CRG Gherkin Plugin JS' );
 
 //The main functionality of this plugin is outputed to Wordpress via a shortcode: [CRG_GherkinShortcode]
 function CRG_GherkinShortcode(){
-
 	CRGautoloader("CRG_GherkinShortcode");
 	$CRG_GherkinShortcode = new CRG_GherkinShortcode;
 	return $CRG_GherkinShortcode->getShortcodeOutput();
