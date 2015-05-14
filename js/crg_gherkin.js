13518b634b97c592795292e3e9aac307e96043fe
Hitman007 <support@customrayguns.com>
2015-05-13 19:12:49 -0700

    Update crg_gherkin.js

--- js/crg_gherkin.js
+++ js/crg_gherkin.js
@@ -1,7 +1,3 @@
 jQuery("dcoument").ready(function(){
-	//alert("jQuery running!");
-	//jQuery('img[src="/wp-content/plugins/crg_gherkin/assets/images/pickle.png"]').css("width","36px");
-	//jQuery('img[src="/wp-content/plugins/crg_gherkin/assets/images/pickle.png"]').css("max-height","16px");
-	//jQuery(".wp-menu-image").css()
-
+	alert("jQuery running. /js/crg_gherkin.js");
 });
