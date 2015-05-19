5cf7c5b3672b982abc39697fb83a4db20695712f
Hitman007 <support@customrayguns.com>
2015-05-19 09:55:34 -0700

    Update CRG_GherkinCPTs.php

--- CRG_GherkinCPTs.php
+++ CRG_GherkinCPTs.php
@@ -4,19 +4,23 @@ Plugin Name: CRG Gherkin Custom Post Types
 Plugin URI: http://customrayguns.com
 Description: CRG Gherkin CPTs
 Version: 1.1
-Author: Custom Ray Guns
+Author: Hitman007
 Author URI: http://customrayguns.com
 Text Domain: CRG_Gherkin_Text_Domain
 */
 
+//load and instantiate our plugin:
 include "classes/CRG_GherkinCPTsPlugin.class.php";
-
 $CRG_GherkinCPTsPlugin = new CRG_GherkinCPTsPlugin;
 
+//Normal Wordpress stuff:
 $CRG_GherkinCPTsPlugin->registerAcitvationHook(__FILE__);
 $CRG_GherkinCPTsPlugin->registerAndEnqueueScripts();
+
+//The plugin's main functionality:
 $CRG_GherkinCPTsPlugin->registerCustomPostTypes();
+
+//A "pickle" is a single BDD statement, and the name of the CPT we use:
 $CRG_GherkinCPTsPlugin->receivePickleIfSubmitted();
-//$CRG_GherkinCPTsPlugin->addFeatureRequestLinksInPluginsAdminSection();
-//$CRG_GherkinCPTsPlugin->registerFeaturesAdminPage();
+
 ?>
